47b5ea6ad96149833be998698d43d534c421d707
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-11-10 15:36:40 +0100

    Created simple functionality to download a calendar

--- src/main/java/com/example/demo/CalendarController.java
+++ src/main/java/com/example/demo/CalendarController.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class CalendarController {
 
-    @GetMapping ("/getSchedule")
-    public void getSchedule(@RequestParam int month, @RequestParam int year){
-        
+    @GetMapping("/getSchedule")
+    public void getSchedule(@RequestParam int month, @RequestParam int year) throws IOException {
+        String address = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + year + "&miesiac=" + month;
+
+        Document doc = Jsoup.connect(address).get();
+
+        System.out.println(doc.toString());
     }
 }
